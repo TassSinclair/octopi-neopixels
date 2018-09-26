57bc7bd6d665c005736bd17088d9eb3b3ca016ce
TassSinclair <andrew.hamilton@thoughtworks.com>
2018-09-26 20:55:32 +1000

    Use state model and simulation to control how we use the world state to interact with the strip.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const shouldMockBoard = () => process.env.BOARD === 'mock'
+const shouldMockBoard = process.env.BOARD === 'mock';
 
-var five = shouldMockBoard ? require('./mocks').five : require('johnny-five');
-var pixel = shouldMockBoard ? require('./mocks').pixel : require('node-pixel');
-var chalk = require('chalk');
-var config = require('./config');
-var octopi = require('./octopi');
+const five = shouldMockBoard ? require('./mocks').five : require('johnny-five');
+const pixel = shouldMockBoard ? require('./mocks').pixel : require('node-pixel');
+const chalk = require('chalk');
+const config = require('./config');
+const octopi = require('./octopi');
+const { Simulation, State, Transition } = require('./state-model');
 
 var opts = {};
 opts.port = process.argv[2] || '';
 
-var board = new five.Board(opts);
-var strip = null;
+const board = new five.Board(opts);
 
 var percentage = 0;
 const strips = [{ pin: 6, length: 8 }];
@@ -24,32 +24,56 @@ function initStrip(board) {
     });
 }
 
-board.on('ready', function () {
+const simulation = new Simulation();
+
+const transitions = [
+    new Transition('idling', (world) => !world.job),
+    new Transition('printing', (world) => world.job),
+]
+
+const idlingState = new State('idling', () => { }, transitions)
+const createPrintingState = (strip) => new State('printing',
+    (world) => {
+
+        percentage = Math.round(world.job.progress.completion);
+        strip.color('#000');
+        var fullLeds = Math.floor(percentage / divisor);
+        var partial = percentage % divisor;
+        for (var i = 0; i < fullLeds; ++i) {
+            strip.pixel(i).color('white');
+        }
+        if (fullLeds < strip.length) {
+            var part = Math.round(128 * (partial / divisor));
+            strip.pixel(fullLeds).color([part, part, part]);
+        }
+        console.log('updating NeoPixels,',
+            percentage, '%,',
+            fullLeds, 'fully illuminated',
+            (fullLeds < strip.length ? 'and 1 partially' : '')
+        );
+        strip.show();
+    },
+    transitions,
+);
+
+const senseWorld = () => Promise.all([
+    octopi.getJob()
+]).then(([job]) => ({
+    job
+}));
+
+board.on('ready', () => {
     console.log('Board: ' + chalk.green('✓'));
-    strip = initStrip(board);
+    const strip = initStrip(board);
 
-    strip.on('ready', function () {
+    strip.on('ready', () => {
         console.log('Strip: ' + chalk.green('✓'));
+        simulation.states.push(idlingState, createPrintingState(strip));
+        simulation.setCurrentState('idling')
 
-        setInterval(function () {
-            octopi('job', function (body) {
-                percentage = Math.round(body.progress.completion);
-                strip.color('#000');
-                var fullLeds = Math.floor(percentage / divisor);
-                var partial = percentage % divisor;
-                for (var i = 0; i < fullLeds; ++i) {
-                    strip.pixel(i).color('white');
-                }
-                if (fullLeds < strip.length) {
-                    var part = Math.round(128 * (partial / divisor));
-                    strip.pixel(fullLeds).color([part, part, part]);
-                }
-                console.log('updating NeoPixels,',
-                    percentage, '%,',
-                    fullLeds, 'fully illuminated',
-                    (fullLeds < strip.length ? 'and 1 partially' : '')
-                );
-                strip.show();
+        setInterval(() => {
+            senseWorld().then(world => {
+                simulation.tick(world);
             });
         }, 1000);
     });
--- octopi.js
+++ octopi.js
@@ -3,22 +3,27 @@ var config = require('./config');
 
 const server = process.env.SERVER ? process.env.SERVER : 'octopi.local';
 
-var getState = function (item, callback) {
-    var stateCall = {
-        url: `http://${server}/api/${item}`,
-        headers: {
-            'X-Api-Key': config.octopiApiKey,
-        }
-    };
-    request(stateCall, function (error, response, body) {
-        if (error) {
-            console.log('error:', error);
-            console.log('statusCode:', response && response.statusCode);
-        }
-        if (body) {
-            callback(JSON.parse(body));
-        }
+function getState(item) {
+    return new Promise((resolve, reject) => {
+        var stateCall = {
+            url: `http://${server}/api/${item}`,
+            headers: {
+                'X-Api-Key': config.octopiApiKey,
+            }
+        };
+        request(stateCall, (error, response, body) => {
+            if (error) {
+                console.log('error:', error);
+                console.log('statusCode:', response && response.statusCode);
+                reject();
+            }
+            if (body) {
+                resolve(JSON.parse(body));
+            }
+        });
     });
 }
 
-module.exports = getState
+module.exports = {
+    getJob: () => getState('job')
+}
